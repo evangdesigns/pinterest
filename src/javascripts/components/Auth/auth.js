5e546293642173f6adeadcbfce320e3cc6e279e8
Evan Grabenstein <evangdesigns@gmail.com>
2019-10-30 20:30:53 -0500

    completed log out fuctionality

--- src/javascripts/components/Auth/auth.js
+++ src/javascripts/components/Auth/auth.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import utilities from '../../helpers/utilities';
-import liBtn from './li_google.jpg';
+import liBtn from './li_google.png';
 
 const signMeIn = () => {
   const provider = new firebase.auth.GoogleAuthProvider();
@@ -10,9 +10,12 @@ const signMeIn = () => {
 };
 
 const loginButton = () => {
-  const domString = `<button id="googleAuth">
-    <img src=${liBtn} alt="Login with Your Google Account">
-  </button>
+  const domString = `
+  <center>
+    <button id="googleAuth" class="btn btn-link">
+      <img src=${liBtn} alt="Login with Your Google Account">
+    </button>
+  </center>
   `;
   utilities.printToDom('auth', domString);
   $('#googleAuth').click(signMeIn);
