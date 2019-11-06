d590bfcd2eadd2efca2239619730f83b0d84e9e2
Evan Grabenstein <evangdesigns@gmail.com>
2019-11-05 19:46:38 -0600

    completed home component

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
+const homeDiv = $('#home');
 const authDiv = $('#auth');
 const boardsDiv = $('#boards');
 const logoutNavBar = $('#logOut');
@@ -9,6 +10,7 @@ const logoutNavBar = $('#logOut');
 const checkLoginStaus = () => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
+      homeDiv.addClass('hide');
       boardsDiv.removeClass('hide');
       logoutNavBar.removeClass('hide');
       authDiv.addClass('hide');
@@ -16,6 +18,7 @@ const checkLoginStaus = () => {
       boardsDiv.addClass('hide');
       logoutNavBar.addClass('hide');
       authDiv.removeClass('hide');
+      homeDiv.removeClass('hide');
     }
   });
 };
