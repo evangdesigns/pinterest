d6921d944fdfa0f3b5b8cf25154b0c1f68b201f3
Evan Grabenstein <evangdesigns@gmail.com>
2019-11-06 19:28:48 -0600

    successfully consled individual board data

--- src/javascripts/components/boards/boards.js
+++ src/javascripts/components/boards/boards.js
@@ -1,12 +1,20 @@
 import './boards.scss';
 import utilities from '../../helpers/utilities';
+import boards from '../../helpers/data/boardsData';
 
 const printBoards = () => {
-  const domString = `<div id="boards">
-    <h1 class="text-center">BOARDS</h1>
-  </div>`;
-
-  utilities.printToDom('boards', domString);
+  boards.getBoards()
+    .then((boardId) => {
+      let domString = `<div id="boards">
+      <h1 class="text-center">BOARDS</h1>
+      </div>`;
+      domString += '<div id="snack-section" class="d-flex flex-wrap justify-content-center">';
+      boardId.forEach((board) => {
+        console.log(board);
+      });
+      utilities.printToDom('boards', domString);
+    })
+    .catch((error) => console.error(error));
 };
 
 export default { printBoards };
