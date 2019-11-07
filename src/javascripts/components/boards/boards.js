8834e3d0a998a4c72b051cf6f619da0da6b7dbd3
Evan Grabenstein <evangdesigns@gmail.com>
2019-11-06 19:44:42 -0600

    printed boards to dom

--- src/javascripts/components/boards/boards.js
+++ src/javascripts/components/boards/boards.js
@@ -5,13 +5,15 @@ import boards from '../../helpers/data/boardsData';
 const printBoards = () => {
   boards.getBoards()
     .then((boardId) => {
-      let domString = `<div id="boards">
-      <h1 class="text-center">BOARDS</h1>
-      </div>`;
-      domString += '<div id="snack-section" class="d-flex flex-wrap justify-content-center">';
+      let domString = '<h1 class="text-center">BOARDS</h1>';
+      domString += '<div id="boards-section" class="d-flex flex-wrap justify-content-center">';
       boardId.forEach((board) => {
-        console.log(board);
+        domString += `<div class="card col-4">
+        <h3 class="text-center">${board.name}</h3>
+        <button type="button" class="btn btn-danger" id="view-${board.id}">VIEW BOARD</button>
+        </div>`;
       });
+      domString += '</div>';
       utilities.printToDom('boards', domString);
     })
     .catch((error) => console.error(error));
