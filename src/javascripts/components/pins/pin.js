c2cbcc718c040ca56d1b2997c76b28c693720ca8
Evan Grabenstein <evangdesigns@gmail.com>
2019-11-09 12:03:14 -0600

    go back complete

--- src/javascripts/components/pins/pin.js
+++ src/javascripts/components/pins/pin.js
@@ -1,17 +1,17 @@
 import './pin.scss';
 
-const pinCard = (boardId) => {
+const pinCard = (pins) => {
   let domString = '';
   domString += `
   <div class="card col-4">
     <div class="card-body">
-    <h3 class="card-title text-center">${boardId.name}</h3>
-    <a href="${boardId.url}" target="_blank">
-      <img src=${boardId.imageUrl} class="card-img-top" alt="${boardId.name}">
+    <h3 class="card-title text-center">${pins.name}</h3>
+    <a href="${pins.url}" target="_blank">
+      <img src=${pins.imageUrl} class="card-img-top" alt="${pins.name}">
     </div>
     <div class="card-footer">
       <center>
-      <button type="submit" class="btn btn-danger btn-block" formaction="" id="${boardId.id}">VIEW</button>
+        <button type="submit" class="btn btn-danger btn-block" formaction="" id="${pins.id}">VIEW</button>
       </center>
       </a>
     </div>
