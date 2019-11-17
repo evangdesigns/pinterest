1b7e92914e8c95f347c210415f9ec2658cd13e5c
Evan Grabenstein <evangdesigns@gmail.com>
2019-11-16 22:42:46 -0600

    added add pin functionality

--- src/javascripts/components/board/board.js
+++ src/javascripts/components/board/board.js
@@ -7,6 +7,46 @@ import './board.scss';
 const boardsDiv = $('#boards');
 const boardDiv = $('#board');
 
+const addPin = (e) => {
+  e.stopImmediatePropagation();
+  const boardId = e.target.id;
+  const newPin = {
+    boardId,
+    name: $('#pinName').val(),
+    imageUrl: $('#pinImage').val(),
+    url: $('#pinLink').val(),
+  };
+  pinData.addNewPin(newPin)
+    .then(() => {
+      $('#uniModal').modal('hide');
+      // eslint-disable-next-line no-use-before-define
+      buildBoard(boardId);
+    })
+    .catch((error) => console.error(error));
+};
+
+const pinModal = (boardId) => {
+  const bId = boardId;
+  const title = `Add A Pin to ${bId}`;
+  const body = `<form>
+    <div class="form-group">
+      <label for="boardName">Board Name</label>
+      <input type="text" class="form-control" id="pinName" placeholder="Enter Pin Name">
+    </div>
+    <div class="form-group">
+      <label for="pinImage">Pin Image</label>
+      <input type="text" class="form-control" id="pinImage" placeholder="Enter Pin Image address">
+    </div>
+    <div class="form-group">
+      <label for="pinLink">Pin Link</label>
+      <input type="text" class="form-control" id="pinLink" placeholder="Enter Pin Link">
+    </div>
+  </form>
+  <button type="button" class="btn btn-danger add-new-pin" id="${bId}">ADD PIN</button>`;
+  utilities.printModal(title, body);
+  $(`#${bId}`).click(addPin);
+};
+
 const deleteFromBoard = (e) => {
   e.stopImmediatePropagation();
   const boardId = e.target.parentElement.id;
@@ -22,7 +62,9 @@ const deleteFromBoard = (e) => {
 const buildBoard = (boardId) => {
   boardsDiv.addClass('hide');
   boardDiv.removeClass('hide');
+  boardDiv.add(`#${boardId}`);
   let domString = `<h1 class="text-center">${boardId}</h1>`;
+  domString += '<center><button type="link" class="btn btn-link add-pin" data-toggle="modal" data-target="#uniModal" id="add-pin">ADD PIN</button></center>';
   domString += `<center>
       <a href="/"class="btn btn-link go-back-boards">BACK TO BOARDS</a>
     </center>
@@ -36,6 +78,7 @@ const buildBoard = (boardId) => {
       domString += '</div>';
       utilities.printToDom('board', domString);
       $('#board').on('click', '.delete-pin', deleteFromBoard);
+      $('#add-pin').click(pinModal(boardId));
     })
     .catch((error) => console.error(error));
 };
